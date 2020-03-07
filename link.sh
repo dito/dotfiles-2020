01c0b8314c0319bbc14fc68f9ef049f3de78ab6b
dito <itodaisuke00@gmail.com>
2020-03-07 20:50:30 +0900

    hammerspoon

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.config/git/ignore
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.hammerspoon ~/.hammerspoon
+ln -nsf ~/dotfiles/.hammerspoon ~/.hammerspoon
