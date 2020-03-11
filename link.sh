bae2c2f850d91fbcc53212b76b3dc5b98fa95d7f
dito <itodaisuke00@gmail.com>
2020-03-12 05:39:42 +0900

    prompt

--- link.sh
+++ link.sh
@@ -1,4 +1,5 @@
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.config/git/ignore
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -nsf ~/dotfiles/.zsh ~/.zsh
 ln -nsf ~/dotfiles/.hammerspoon ~/.hammerspoon
